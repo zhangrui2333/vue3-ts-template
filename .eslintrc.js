e1dff3ed8266b15a9dc093e18ced348a1b324f59
zhangrui <395985079@qq.com>
2023-02-22 18:24:51 +0800

    feat: 初始化

---
+++ .eslintrc.js
@@ -0,0 +1,122 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-airbnb-base',
+    '@vue/typescript/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue-scoped-css/base',
+    // 1.继承.prettierrc.js文件规则  2.开启rules的 "prettier/prettier": "error"  3.eslint fix的同时执行prettier格式化
+    'plugin:prettier/recommended'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx']
+  },
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/prefer-default-export': 'off'
+    // 'import/no-unresolved': 'off',
+    // 'import/extensions': 'off'
+    // 'import/no-absolute-path': 'off'
+  }
+
+  //   'no-console': 'off',
+  //   'no-continue': 'off',
+  //   'no-restricted-syntax': 'off',
+  //   'no-plusplus': 'off',
+  //   'no-param-reassign': 'off',
+  //   'no-shadow': 'off',
+  //   'guard-for-in': 'off',
+  //   'prettier/prettier': 'off', // 解决replace with
+
+  //   'import/extensions': 'off',
+  //   'import/no-unresolved': 'off',
+  //   'import/no-extraneous-dependencies': ['error', { devDependencies: true },
+  //   'import/prefer-default-export': 'off',
+  //   'import/first': 'off', // https://github.com/vuejs/vue-eslint-parser/issues/58
+  //   '@typescript-eslint/no-explicit-any': 'off',
+  //   '@typescript-eslint/explicit-module-boundary-types': 'off',
+  //   'vue/first-attribute-linebreak': 0,
+
+  //   '@typescript-eslint/no-unused-vars': [
+  //     'error',
+  //     {
+  //       argsIgnorePattern: '^_',
+  //       varsIgnorePattern: '^_'
+  //     }
+  //   ],
+  //   'no-unused-vars': [
+  //     'error',
+  //     {
+  //       argsIgnorePattern: '^_',
+  //       varsIgnorePattern: '^_'
+  //     }
+  //   ],
+  //   'no-use-before-define': 'off',
+  //   '@typescript-eslint/no-use-before-define': 'off',
+  //   '@typescript-eslint/ban-ts-comment': 'off',
+  //   '@typescript-eslint/ban-types': 'off',
+  //   'class-methods-use-this': 'off' // 因为AxiosCancel必须实例化而能静态化所以加的规则，如果有办法解决可以取消
+  // },
+  // overrides: [
+  //   {
+  //     files: ['*.vue'],
+  //     rules: {
+  //       'vue/component-name-in-template-casing': [2, 'kebab-case'],
+  //       'vue/require-default-prop': 0,
+  //       'vue/multi-word-component-names': 0,
+  //       'vue/no-reserved-props': 0,
+  //       'vue/no-v-html': 0,
+  //       'vue-scoped-css/enforce-style-type': ['error', { allows: ['scoped'] }]
+  //     }
+  //   },
+  //   {
+  //     files: ['*.ts', '*.tsx'], // https://github.com/typescript-eslint eslint-recommended
+  //     rules: {
+  //       'constructor-super': 'off', // ts(2335) & ts(2377)
+  //       'getter-return': 'off', // ts(2378)
+  //       'no-const-assign': 'off', // ts(2588)
+  //       'no-dupe-args': 'off', // ts(2300)
+  //       'no-dupe-class-members': 'off', // ts(2393) & ts(2300)
+  //       'no-dupe-keys': 'off', // ts(1117)
+  //       'no-func-assign': 'off', // ts(2539)
+  //       'no-import-assign': 'off', // ts(2539) & ts(2540)
+  //       'no-new-symbol': 'off', // ts(2588)
+  //       'no-obj-calls': 'off', // ts(2349)
+  //       'no-redeclare': 'off', // ts(2451)
+  //       'no-setter-return': 'off', // ts(2408)
+  //       'no-this-before-super': 'off', // ts(2376)
+  //       'no-undef': 'off', // ts(2304)
+  //       'no-unreachable': 'off', // ts(7027)
+  //       'no-unsafe-negation': 'off', // ts(2365) & ts(2360) & ts(2358)
+  //       'no-var': 'error', // ts transpiles let/const to var, so no need for vars any more
+  //       'prefer-const': 'error', // ts provides better types with const
+  //       'prefer-rest-params': 'error', // ts provides better types with rest args over arguments
+  //       'prefer-spread': 'error', // ts transpiles spread to apply, so no need for manual apply
+  //       'valid-typeof': 'off' // ts(2367)
+  //     }
+  //   }
+  // ]
+};
